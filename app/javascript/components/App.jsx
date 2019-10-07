e5ddda9abe6314debab30ea0751041245e0eae77
andy_hardy <aohardy@gmail.com>
2019-10-06 22:14:26 -0600

    set up react front end and a basic home route

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Routes from '../routes/Index';
+
+const App = props => {
+    return (
+        <>
+            <Routes />
+        </>
+    )
+}
+
+export default App;
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from '@material-ui/core';
+
+const Home = props => {
+    return (
+        <Container
+            
+        >
+            
+        </Container>
+    )
+};
+
+export default Home;
